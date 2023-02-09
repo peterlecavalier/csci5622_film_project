1a3f073729084f96e9888329bb35682f1a5a5fed
Peter LeCavalier <pele6150@colorado.edu>
2023-02-08 21:53:49 -0700

    Updated IMDb scraping to multiprocess

--- new_imdb_scraping.py
+++ new_imdb_scraping.py
@@ -3,18 +3,17 @@ import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 import json
+from tqdm.contrib.concurrent import process_map
+#from p_tqdm import p_map
 
-df = pd.read_csv('./data/initial_cleaned_data.csv', index_col=0)
-imdb_ids = list(df['imdb_id'])
+def pull_imdb(id):
+    rating = ""
+    budget = ""
+    revenue = ""
 
-imdb_data = {}
-
-# These headers help with the requests
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'}
-
-for idx, id in enumerate(tqdm(imdb_ids)):
+    # These headers help with the requests
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'}
     if not pd.isna(id):
-        imdb_data[id] = {}
         imdb_page = requests.get(f'https://www.imdb.com/title/{id}/', headers=headers)
         if not imdb_page.ok:
             print(f"{id} failed")
@@ -24,16 +23,29 @@ for idx, id in enumerate(tqdm(imdb_ids)):
             budget_li = soup.find('li', attrs={'data-testid': 'title-boxoffice-budget'})
             rev_li = soup.find('li', attrs={'data-testid': 'title-boxoffice-cumulativeworldwidegross'})
             if rating_div is not None:
-                imdb_data[id]['imdb_rating'] = rating_div.contents[0].contents[0]
+                rating = rating_div.contents[0].contents[0]
             if budget_li is not None:
-                imdb_data[id]['imdb_budget'] = budget_li.find('label').contents[0]
+                budget = budget_li.find('label').contents[0]
             if rev_li is not None:
-                imdb_data[id]['imdb_revenue'] = rev_li.find('label').contents[0]
+                revenue = rev_li.find('label').contents[0]
         except:
             pass
-    if idx % 2000 == 0:
-        with open("./data/imdb_data.json", "w") as f:
-            json.dump(imdb_data, f)
-# Save the final file
-with open("./data/imdb_data.json", "w") as f:
-    json.dump(imdb_data, f)
+
+        imdb_data = f"id={id}&rating={rating}&budget={budget}&revenue={revenue}"
+        return imdb_data
+
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('./data/post_vis_cleaned_data.csv', index_col=0)
+    imdb_ids = list(df['imdb_id'])
+    final_list = process_map(pull_imdb, imdb_ids, chunksize=100)
+
+    save_dict = {}
+    for i in final_list:
+        key = i.keys()[0]
+        save_dict[key] = i[key]
+
+    # Save the final file
+    with open("./data/imdb_data.json", "w") as f:
+        json.dump(save_dict, f)
